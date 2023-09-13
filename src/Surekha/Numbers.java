dc9685fdf4a3cae78043093b8a5f3b77235d988e
govin <govin@Phantom>
2023-09-13 12:34:42 +0530

    second commit

---
+++ src/Surekha/Numbers.java
@@ -0,0 +1,12 @@
+package Surekha;
+import java.util.*;
+public class Numbers {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=n;i>=0;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
